bb8400bb9f834155f974a7dbbbeea58eb06cadc5
tyler <tyler@Deb.myguest.virtualbox.org>
2023-10-14 23:54:39 -0400

    formatting and moving stuff around

--- lua/tyler/plugins.lua
+++ lua/tyler/plugins.lua
@@ -1,8 +1,17 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- theme
+    use { 
+        "catppuccin/nvim", 
+        as = "catppuccin" ,
+        config = function()
+            vim.cmd("colorscheme catppuccin-mocha")
+        end
+    }
 
     -- file finder
     use {
@@ -16,6 +25,7 @@ return require('packer').startup(function(use)
         requires = { {'nvim-telescope/telescope.nvim'} }
     }
 
+    -- Terminal
     use {
         'NvChad/nvterm',
         config = function()
@@ -23,23 +33,14 @@ return require('packer').startup(function(use)
         end,
     }
 
-	-- theme
-	use { 
-		"catppuccin/nvim", 
-		as = "catppuccin" ,
-		config = function()
-			vim.cmd("colorscheme catppuccin-mocha")
-		end
-	}
-
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
-	use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} }
-	use {"nvim-treesitter/nvim-treesitter-context"}
-	use {'mbbill/undotree'}
+    use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} }
+    use {"nvim-treesitter/nvim-treesitter-context"}
+    use {'mbbill/undotree'}
 
     -- Vim helpers
     use {
@@ -55,43 +56,44 @@ return require('packer').startup(function(use)
         end
     }
 
-	-- file stuff
-	use {'nvim-tree/nvim-tree.lua'}
-	use {'nvim-tree/nvim-web-devicons'}
+    -- file stuff
+    use {'nvim-tree/nvim-tree.lua'}
+    use {'nvim-tree/nvim-web-devicons'}
 
-	use {'lewis6991/gitsigns.nvim'}
-	use {'romgrk/barbar.nvim'}
+    use {'lewis6991/gitsigns.nvim'}
+    use {'romgrk/barbar.nvim'}
 
-	-- git
-	use {'tpope/vim-fugitive'}
+    -- git
+    use {'tpope/vim-fugitive'}
 
-	-- LSP and mason
-	use {
-		"williamboman/mason.nvim"
-	}
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
+    -- LSP and mason
+    use {
+        "williamboman/mason.nvim",
+    }
+
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
     use {'tomiis4/Hypersonic.nvim'}
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment these if you want to manage LSP servers from neovim
-			-- {'williamboman/mason.nvim'},
-			-- {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            -- {'williamboman/mason.nvim'},
+            -- {'williamboman/mason-lspconfig.nvim'},
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 
     use {
         "olexsmir/gopher.nvim",
